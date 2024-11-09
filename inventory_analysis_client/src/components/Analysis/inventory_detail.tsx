31ef5e83287370b1c128afe90d62ffb151a5b358
Husnul Abid <habid96@gmail.com>
2024-11-09 21:41:51 +0200

    detail list done

--- inventory_analysis_client/src/components/Analysis/inventory_detail.tsx
+++ inventory_analysis_client/src/components/Analysis/inventory_detail.tsx
@@ -7,30 +7,29 @@ import { FLASK_API_URL } from '../../constants';
 
 type Attributes = {
     primary: {
-      [key: string]: string;
+        [key: string]: string;
     };
     secondary: {
-      [key: string]: string;
+        [key: string]: string;
     };
-  };
-  
-  type Data = {
+};
+
+type Data = {
     attributes: Attributes;
     date_time: string;
     comments: string;
-    building: string
-  };
-  
-  interface TableProps {
-    data: Data;
-  }
+    building: string;
+    maintenance_required: number;
+    high_priority: number
+    equipment_name: string
+};
 
 
 const flask_api_project_url = FLASK_API_URL;
 
 const InventoryDetail: React.FC = () => {
     const { record_id } = useParams();
-    const [itemData, setItemData] = useState(null);
+    const [itemData, setItemData] = useState<Data>();
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
 
@@ -52,7 +51,7 @@ const InventoryDetail: React.FC = () => {
                 setError(error.message);
                 setLoading(false);
             });
-        
+
     }, [record_id]);
 
 
@@ -61,10 +60,102 @@ const InventoryDetail: React.FC = () => {
             <div className="mainBody">
                 <div>
                     <h5>Inventory Details</h5>
-                    <pre>{JSON.stringify(itemData, null, 2)}</pre>
+                    <h2>{itemData?.equipment_name}</h2>
                 </div>
                 <div>
+
+                    <br></br>
+                    <br></br>
+                    <br></br>
+
+                    <h4 style={{ textAlign: 'left', marginLeft: '40px' }} >General Information</h4>
+
+                    <table className="table_2">
+                        <thead>
+                            <tr>
+                                <th style={{ width: '250px' }}>Key</th>
+                                <th style={{ width: '250px' }}>Value</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Building</td>
+                                <td>{itemData?.building}</td>
+                            </tr>
+
+                            <tr>
+                                <td>Date Time</td>
+                                <td>{itemData?.date_time}</td>
+                            </tr>
+
+                            <tr>
+                                <td>Comments</td>
+                                <td>{itemData?.comments}</td>
+                            </tr>
+
+                            <tr>
+                                <td>Required Maintenence</td>
+                                <td>{itemData?.maintenance_required === 1 ? 'True' : 'False'}</td>
+                            </tr>
+
+                            <tr>
+                                <td>High Priority</td>
+                                <td>{itemData?.high_priority === 1 ? 'True' : 'False'}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <br></br>
+                    <br></br>
+                    <br></br>
+
+                    <h4 style={{ textAlign: 'left', marginLeft: '40px' }} >Primary Information</h4>
+
+                    <table className="table_2">
+                        <thead>
+                            <tr>
+                                <th style={{ width: '250px' }}>Key</th>
+                                <th style={{ width: '250px' }}>Value</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {itemData != null && itemData.attributes.primary &&
+                                Object.entries(itemData.attributes.primary).map(([key, value]) => (
+                                    <tr key={`primary-${key}`}>
+                                        <td>{key}</td>
+                                        <td>{value}</td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+
+                    <br></br>
+                    <br></br>
+                    <br></br>
                     
+                    <h4 style={{ textAlign: 'left', marginLeft: '40px' }} >Secondary Information</h4>
+
+                    <table className="table_2">
+                        <thead>
+                            <tr>
+                                <th style={{ width: '250px' }}>Key</th>
+                                <th style={{ width: '250px' }}>Value</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {itemData != null && itemData.attributes.secondary &&
+                                Object.entries(itemData.attributes.secondary).map(([key, value]) => (
+                                    <tr key={`secondary-${key}`}>
+                                        <td>{key}</td>
+                                        <td>{value}</td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+
+
                 </div>
             </div>
         </div>
